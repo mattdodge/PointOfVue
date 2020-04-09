cf705bb5be9e876cfbcf768adf87bdbcb2253bf5
Matt Dodge <mattedgod@gmail.com>
2020-04-09 10:43:01 -0700

    Allow method binding in vue ctx

--- pointofvue/__init__.py
+++ pointofvue/__init__.py
@@ -1,3 +1,3 @@
 from .context import VueContext
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
--- setup.py
+++ setup.py
@@ -1,20 +1,29 @@
+import re
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-reqs = [
-]
+with open('requirements.txt') as f:
+    reqs = f.read().splitlines()
+
+with open('yahoofantasy/__init__.py', 'r') as fd:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+
 
 setuptools.setup(
     name="pointofvue",
-    version="0.0.1",
+    version=version,
     author="Matt Dodge",
     author_email="mattedgod@gmail.com",
     description="A simple, opinionated way to incorporate VueJS into Django apps",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=reqs,
+    data_files=[
+        'templates',
+    ],
     url="https://github.com/mattdodge/PointOfVue",
     packages=setuptools.find_packages(),
     classifiers=[
