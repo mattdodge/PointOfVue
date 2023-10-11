5c5432a0d793c375c1e5a4506d60f36ac49557c2
Matt Dodge <mattedgod@gmail.com>
2023-10-10 21:20:03 -0700

    support vue3

--- pointofvue/templatetags/pointofvue.py
+++ pointofvue/templatetags/pointofvue.py
@@ -1,10 +1,13 @@
 from django import template
 from ..context import VueContext
+from ..settings import get_setting
 
 register = template.Library()
 
+script_name = 'script_vue3.html' if get_setting('VUE_VERSION', '2') == '3' else 'script.html'
 
-@register.inclusion_tag('script.html', name='vue')
+
+@register.inclusion_tag(script_name, name='vue')
 def vue(vue_ctx):
     if not isinstance(vue_ctx, VueContext):
         raise TypeError("Argument to 'vue' template must be a VueContext")
